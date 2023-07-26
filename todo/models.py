6f100d25aa60fd0f31d14d263ebefdba23d3f595
Vlad <vladislav.zhyla@gmail.com>
2023-07-26 15:38:10 +0300

    Created app todo. Created urls, models and views for this app

---
+++ todo/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    content = models.CharField(max_length=255)
+    datetime = models.DateTimeField(auto_now_add=True)
+    deadline = models.DateTimeField(null=True, blank=True)
+    is_done = models.BooleanField()
+    tags = models.ManyToManyField(to=Tag, related_name="tasks")
+
+    class Meta:
+        ordering = ["is_done", "-datetime"]
+
